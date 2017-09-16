e212faf824d54dfc6e1ccd6ec205ebe83907cd81
Isaac Simon <ijsimon@sbcglobal.net>
2017-09-16 22:43:19 +0000

    You can create, read, update, and delete wikis

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,8 +8,28 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
   end
 
+  def create
+ # #9
+    @wiki = Wiki.new()
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.user = current_user
+
+ # #10
+    if @wiki.save
+ # #11
+       flash[:notice] = "Wiki was saved."
+       redirect_to wikis_path
+    else
+ # #12
+       flash.now[:alert] = "There was an error saving the wiki. Please try again."
+       render :new
+    end
+  end
+  
   def edit
     @wiki = Wiki.find(params[:id])
   end
@@ -26,5 +46,18 @@ class WikisController < ApplicationController
      flash.now[:alert] = "There was an error saving the wiki. Please try again."
      render :edit
    end
+  end
+  
+  def destroy
+     @wiki = Wiki.find(params[:id])
+ 
+ # #8
+     if @wiki.destroy
+       flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+       redirect_to wikis_path
+     else
+       flash.now[:alert] = "There was an error deleting the wiki."
+       render :show
+     end
   end
 end
